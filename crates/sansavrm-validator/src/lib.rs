2dccb5079b81af55fc0319c496483ab92452fb1c
mayusaki3 <masayuki@fa2.so-net.ne.jp>
2026-05-08 15:12:09 +0900

    Export custom parameter validator

--- crates/sansavrm-validator/src/lib.rs
+++ crates/sansavrm-validator/src/lib.rs
@@ -1,11 +1,13 @@
 // lib.rs
 
+pub mod custom_parameter_api;
 pub mod result;
 pub mod validate;
 pub mod validate_api;
 pub mod mujoco_api;
 pub mod vrm_validation;
 
+pub use custom_parameter_api::*;
 pub use result::*;
 pub use validate::*;
 pub use validate_api::*;
